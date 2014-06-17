62711be86256d155ae4bfe941f2984f9034b0168
jwarburton@gmail.com <jwarburton@gmail.com>
2014-06-17 21:35:16 +1000

    peppa works now

--- peppa.pl
+++ peppa.pl
@@ -10,12 +10,20 @@ $|++;
 # does not have a mp4 file
 #
 
-my ($ts);
+my ($ts, $mp4);
 
 my $PeppaDir='/Volumes/video/TV\ Shows/Peppa\ Pig';
 
 
 my @TS=glob("$PeppaDir/*ts");
 foreach $ts (@TS) {
-    print "$ts\n";
+    $mp4 = $ts;
+    $mp4 =~ s/\.ts$/\.mp4/;
+    if (-f $mp4) {
+        print "Skipping - we have $mp4\n";
+    }
+    else {
+        print "/Applications/HandBrakeCLI --preset iPad --input $ts --output $mp4";
+        `/Applications/HandBrakeCLI --preset iPad --input \"$ts\" --output \"$mp4\"`
+    }
 }
