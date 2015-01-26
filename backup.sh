619f91fdb19794117652b70d9b00190af44333e3
jwarburton@gmail.com <jwarburton@gmail.com>
2015-01-26 15:02:48 +1100

    backup for volumes with space in name

--- backup.sh
+++ backup.sh
@@ -2,6 +2,14 @@
 
 # NAS true crypt -- /Volumes/Untitled
 # John's off site drive -- /Volumes/NO\ NAME/truecrypt-backups
+
+NAS="/Volumes/Untitled"
+EXTERNAL="/Volumes/Untitled 1"
+
 echo "Start: `date`" >> backup.log
-rsync -av --modify-window=3601 --delete /Volumes/Untitled/ /Volumes/NO\ NAME/truecrypt-backups/ >> backup.log  2>&1
+#rsync -av --modify-window=3601 --delete /Volumes/Untitled/ /Volumes/NO\ NAME/truecrypt-backups/ >> backup.log  2>&1
+
+rsync -av --modify-window=3601 --delete $NAS/ "$EXTERNAL"/ >> backup.log  2>&1
+
 echo "Finish: `date`" >> backup.log
+
