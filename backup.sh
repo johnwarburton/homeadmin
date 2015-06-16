50c667e52c7dd27352f57d33e70f343487fba459
jwarburton@gmail.com <jwarburton@gmail.com>
2015-06-16 22:09:03 +1000

    backup to new volume names, and do itunes as well

--- backup.sh
+++ backup.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-# NAS true crypt -- /Volumes/Untitled
+# NAS true crypt -- /Volumes/truecrypt
 # John's off site drive -- /Volumes/NO\ NAME/truecrypt-backups
 
-NAS="/Volumes/Untitled"
+NAS="/Volumes/truecrypt"
 EXTERNAL="/Volumes/Untitled 1"
+EXTERNAL="/Volumes/NO\ NAME/truecrypt-backups"
+ITUNES="/Volumes/NAS/iTunes"
+EXT_ITUNES="/Volumes/NO\ NAME/iTunes"
 
 echo "Start: `date`" >> backup.log
 #rsync -av --modify-window=3601 --delete /Volumes/Untitled/ /Volumes/NO\ NAME/truecrypt-backups/ >> backup.log  2>&1
 
 rsync -av --modify-window=3601 --delete $NAS/ "$EXTERNAL"/ >> backup.log  2>&1
+rsync -av --modify-window=3601 --delete $ITUNES/ "$EXT_ITUNES"/ >> backup.log  2>&1
 
 echo "Finish: `date`" >> backup.log
 
