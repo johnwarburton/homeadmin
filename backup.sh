c3bf764bc70eb41b266c5d8e5cca310199f178a2
jwarburton@gmail.com <jwarburton@gmail.com>
2015-06-16 22:51:49 +1000

    struggling with spaces and shell escapes. exclude apps

--- backup.sh
+++ backup.sh
@@ -12,8 +12,8 @@ EXT_ITUNES="/Volumes/NO\ NAME/iTunes"
 echo "Start: `date`" >> backup.log
 #rsync -av --modify-window=3601 --delete /Volumes/Untitled/ /Volumes/NO\ NAME/truecrypt-backups/ >> backup.log  2>&1
 
-rsync -av --modify-window=3601 --delete $NAS/ "$EXTERNAL"/ >> backup.log  2>&1
-rsync -av --modify-window=3601 --delete $ITUNES/ "$EXT_ITUNES"/ >> backup.log  2>&1
+rsync -av --modify-window=3601 --exclude='Mobile Applications' --delete $ITUNES/ /Volumes/NO\ NAME/iTunes >> backup.log  2>&1
+rsync -av --modify-window=3601 --delete $NAS/ /Volumes/NO\ NAME/truecrypt-backups >> backup.log  2>&1
 
 echo "Finish: `date`" >> backup.log
 
